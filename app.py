07af2b8390df8d8e2f0840a6561fe87c9f3421bd
Jefferson <jeramirezca@unal.edu.co>
2025-02-06 21:12:39 -0500

    port change

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from models.products import db
 from controllers.product_controller import * 
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123@172.17.0.3:3306/swarch2024ii_db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123@172.17.0.2:3306/swarch2024ii_db' 
 db.init_app(app) 
 
 app.register_blueprint(product_blueprint) 
 
 if __name__ == '__main__': 
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=4200)
